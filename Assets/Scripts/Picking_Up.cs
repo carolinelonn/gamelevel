85cd6e72ee9d4314dcd83a07b94bec829366d9ba
Sander <sanderb0ss@live.nl>
2017-10-26 08:51:11 +0200

    Level added

---
+++ Assets/Scripts/Picking_Up.cs
@@ -0,0 +1,48 @@
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using  UnityEngine.SceneManagement;
+
+public class Picking_Up: MonoBehaviour
+    {
+
+    private int count;
+    public Text countText;
+    public string sceneName;
+
+
+       void Start()
+        {
+        count = 0;
+        SetCountText();
+    }
+
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+    
+        if (other.gameObject.CompareTag("item_winebottle"))
+            {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+
+        }
+        if (other.gameObject.CompareTag("Police"))
+        {
+            SceneManager.LoadScene(sceneName);
+
+        }
+        if (count == 13)
+        {
+            countText.text = "victory";
+
+        }
+    }
+
+     void SetCountText()
+    {
+        countText.text = "count:" + count.ToString();
+    }
+    }
---
+++ Assets/Scripts/Police.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Police : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public int moveDirection;
+    public float moveTimer;
+    public float moveDuration;
+    public bool moves;
+    private Animator anim;
+    private bool PoliceMoving;
+    private Vector2 LastMoveX;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        moveSpeed = 1;
+        moveDirection = 0;
+        moveTimer = Random.Range(1, 6);
+        moveDuration = 1.2f;
+        moves = false;
+  
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moveTimer = moveTimer - Time.deltaTime;
+        //Debug.Log("timer" + timer);
+        if (moveTimer <= 0)
+        {
+            if (moveDirection == 1)
+            {
+                moveDirection = -1;
+            }
+            else
+            {
+                moveDirection = 1;
+            }
+
+            //Debug.Log("moveDirection" + moveDirection);
+            moveTimer = Random.Range(1, 6);
+            //Debug.Log("moveTimer" + moveTimer);
+            moveDuration = 1.2f;
+        }
+
+        if (moveDirection > 0.5f || moveDirection < -0.5f)
+        {
+            moveDuration = moveDuration - Time.deltaTime;
+            if (moveDuration > 0)
+            {
+                moves = true;
+                //Debug.Log("moves1" + moves);
+                transform.Translate(new Vector3(moveDirection * moveSpeed * Time.deltaTime, 0f, 0f));
+                PoliceMoving= true;
+                
+            }
+            else
+            {
+                PoliceMoving = false;
+                moves = false;
+                //Debug.Log("moves2" + moves);
+            }
+        }
+
+        anim.SetBool("moves", moves);
+        anim.SetFloat("MoveX", moveDirection);
+        anim.SetBool("PoliceMoving", PoliceMoving);
+       
+
+    }
+}
